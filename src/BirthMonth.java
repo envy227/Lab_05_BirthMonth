a63c36977012a405487771e6f60bd5b28bd6bfac
khushi <khushi.devadiga227@gmail.com>
2025-09-18 23:10:51 +0530

    Initial commit

---
+++ src/BirthMonth.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+
+public class BirthMonth {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int month = 0;
+        String trash = "";
+
+        System.out.print("Enter your birth month (1-12): ");
+
+        if (in.hasNextInt()) {
+            month = in.nextInt();
+            in.nextLine();
+
+            if (month >= 1 && month <= 12) {
+                System.out.println("Your birth month is: " + month);
+            } else {
+                System.out.println("You entered an incorrect month value: " + month);
+            }
+        } else {
+            trash = in.nextLine();
+            System.out.println("Invalid input: " + trash);
+        }
+    }
+}
